5716ceead051997f388346a2f707541a8bfddf39
BleedingXiko <66162192+BleedingXiko@users.noreply.github.com>
2023-11-13 16:14:46 -0800

    Think this works now

--- addons/neural_net/Scripts/QLearning/DeepQ.gd
+++ addons/neural_net/Scripts/QLearning/DeepQ.gd
@@ -20,37 +20,37 @@ var is_learning: bool = true
 var previous_state: Array = []
 var previous_action: int
 
-func _init(n_features: int, n_action_spaces: int, _is_learning: bool = true) -> void:
-    observation_space = n_features  # Number of features in the state
-    action_spaces = n_action_spaces
-    is_learning = _is_learning
-    
-    # Initialize the neural network with the number of features as input nodes
-    neural_network = NeuralNetwork.new(n_features, 64, action_spaces)
+func _init(n_features: int, n_nodes: int, n_action_spaces: int, _is_learning: bool = true) -> void:
+	observation_space = n_features  # Number of features in the state
+	action_spaces = n_action_spaces
+	is_learning = _is_learning
+	
+	# Initialize the neural network with the number of features as input nodes
+	neural_network = NeuralNetwork.new(n_features, n_nodes, action_spaces)
 
 func predict(current_states: Array, reward_of_previous_state: float) -> int:
-    var current_q_values = neural_network.predict(current_states)
-    
-    if is_learning and previous_state.size() != 0:
-        var old_q_value = neural_network.predict(previous_state)[previous_action]
-        var max_future_q = max(current_q_values)
-        var target_q_value = reward_of_previous_state + discounted_factor * max_future_q
-        var target_q_values = neural_network.predict(previous_state)
-        target_q_values[previous_action] = target_q_value
-        neural_network.train(previous_state, target_q_values)
-    
-    var action_to_take: int
-    if randf() < exploration_probability:
-        action_to_take = randi() % action_spaces
-    else:
-        action_to_take = current_q_values.index_of_max()
+	var current_q_values = neural_network.predict(current_states)
+	
+	if is_learning and previous_state.size() != 0:
+		var old_q_value = neural_network.predict(previous_state)[previous_action]
+		var max_future_q = current_q_values.max()
+		var target_q_value = reward_of_previous_state + discounted_factor * max_future_q
+		var target_q_values = neural_network.predict(previous_state)
+		target_q_values[previous_action] = target_q_value
+		neural_network.train(previous_state, target_q_values)
+	
+	var action_to_take: int
+	if randf() < exploration_probability:
+		action_to_take = randi() % action_spaces
+	else:
+		action_to_take = current_q_values.find(current_q_values.max()) # needs the indexnot the value
 
-    if is_learning:
-        previous_state = current_states
-        previous_action = action_to_take
-    
-    steps_completed += 1
-    if steps_completed % decay_per_steps == 0:
-        exploration_probability = max(min_exploration_probability, exploration_probability - exploration_decreasing_decay)
-    
-    return action_to_take
+	if is_learning:
+		previous_state = current_states
+		previous_action = action_to_take
+	
+	if steps_completed % decay_per_steps == 0:
+		exploration_probability = max(min_exploration_probability, exploration_probability - exploration_decreasing_decay)
+	
+	steps_completed += 1
+	return action_to_take
