97b1a62d8e829fdcae30142ffca2a185853b7943
BleedingXiko <66162192+BleedingXiko@users.noreply.github.com>
2023-11-13 19:33:04 -0800

    Update nna.gd

--- Demo/NNA/nna.gd
+++ Demo/NNA/nna.gd
@@ -1,12 +1,42 @@
 extends Node2D
+var ACTIVATIONS: Dictionary = {
+	"SIGMOID": {
+		"function": Callable(Activation, "sigmoid"),
+		"derivative": Callable(Activation, "dsigmoid")
+	},
+	"RELU": {
+		"function": Callable(Activation, "relu"),
+		"derivative": Callable(Activation, "drelu")
+	},
+	"TANH": {
+		"function": Callable(Activation, "tanh_"),
+		"derivative": Callable(Activation, "dtanh")
+	},
+	"ARCTAN": {
+		"function": Callable(Activation, "arcTan"),
+		"derivative": Callable(Activation, "darcTan")
+	},
+	"PRELU": {
+		"function": Callable(Activation, "prelu"),
+		"derivative": Callable(Activation, "dprelu")
+	},
+	"ELU": {
+		"function": Callable(Activation, "elu"),
+		"derivative": Callable(Activation, "delu")
+	},
+	"SOFTPLUS": {
+		"function": Callable(Activation, "softplus"),
+		"derivative": Callable(Activation, "dsoftplus")
+	}
+}
+
 
 var nnas: NeuralNetworkAdvanced
+
 func _ready() -> void:
-	nnas = NeuralNetworkAdvanced.new(2, [6,3,2], 1)
-	nnas.hidden_func = nnas.ACTIVATIONS.TANH
-	nnas.output_func = nnas.ACTIVATIONS.SIGMOID
+	nnas = NeuralNetworkAdvanced.new(2, [6,3,2], 1, ACTIVATIONS.RELU, ACTIVATIONS.TANH)
 	nnas.learning_rate = 0.01
-	
+
 
 func _physics_process(delta: float) -> void:
 	nnas.train([0,0], [0])
