d95f8d197c462234e75d57e6fe3da161173afe2a
BleedingXiko <66162192+BleedingXiko@users.noreply.github.com>
2023-11-14 22:15:14 -0800

    .

--- Demo/NN/Demo.gd
+++ Demo/NN/Demo.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 
 func _input(event):
-	if event.is_action_pressed("ui_down"):
+	if event.is_action_pressed("ui_up"):
 		$Neural_Net.best_nn.save("./nn.data")
 	if event.is_action_pressed("ui_down"):
 		$Neural_Net.best_nn.load("./nn.data")
